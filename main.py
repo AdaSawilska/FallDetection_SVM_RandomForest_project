017a1e569a9fae0505740219cb7033d7c7537b94
AdaSawilska <01143808@pw.edu.pl>
2023-05-21 00:49:28 +0200

    each signal parameters in dataframe

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import scipy
 from scipy import signal
 from scipy.fft import fft, fftfreq
@@ -6,7 +7,9 @@ import numpy as np
 import pandas as pd
 import glob
 import matplotlib.pyplot as plt
-import pylab as py
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
 
 
 # reads the csv files to dataFrame
@@ -33,6 +36,56 @@ def classChange(df):
     return df
 
 
+# find feature for all signal together
+def find_global_features(data_files):
+    global_description_x = pd.DataFrame(data=[], index=[
+        "count", "mean", "std", "min", "25%", "50%", "75%", "max"], columns=[])
+    global_description_y = pd.DataFrame(data=[], index=[
+        "count", "mean", "std", "min", "25%", "50%", "75%", "max"], columns=[])
+    global_description_z = pd.DataFrame(data=[], index=[
+        "count", "mean", "std", "min", "25%", "50%", "75%", "max"], columns=[])
+    for filename in data_files:
+        df = pd.read_csv(filename, names=['x', 'y', 'z', 'time'])
+        description_i = df.describe()
+        global_description_x[filename] = description_i["x"]
+        global_features_x = global_description_x.mean(axis=1)
+
+        global_description_y[filename] = description_i["y"]
+        global_features_y = global_description_y.mean(axis=1)
+
+        global_description_z[filename] = description_i["z"]
+        global_features_z = global_description_z.mean(axis=1)
+
+    return global_features_x, global_features_y, global_features_z
+
+
+def find_local_features(df, filename, signals_dataframe):
+    description_i = df.describe()
+    signals_dataframe['mean_x'][filename] = description_i['x']['mean']
+    signals_dataframe['std_x'][filename] = description_i['x']['std']
+    signals_dataframe['mean_y'][filename] = description_i['y']['mean']
+    signals_dataframe['std_y'][filename] = description_i['y']['std']
+    signals_dataframe['mean_z'][filename] = description_i['z']['mean']
+    signals_dataframe['std_z'][filename] = description_i['z']['std']
+    return signals_dataframe
+
+
+def normalize(data, global_features_x, global_features_y, global_features_z):
+    mean_x = float(global_features_x['mean'])
+    std_x = float(global_features_x['std'])
+    mean_y = float(global_features_y['mean'])
+    std_y = float(global_features_y['std'])
+    mean_z = float(global_features_z['mean'])
+    std_z = float(global_features_z['std'])
+
+    normalised_signal = pd.DataFrame(columns=["x", "y", "z", "time"])
+    normalised_signal["x"] = (data["x"] - mean_x) / std_x
+    normalised_signal["y"] = (data["y"] - mean_y) / std_y
+    normalised_signal["z"] = (data["z"] - mean_z) / std_z
+    normalised_signal['time'] = data['time']
+    return normalised_signal
+
+
 # filter signal
 def filter(order, fs, cutoff):
     nyq = 0.5 * fs
@@ -40,15 +93,17 @@ def filter(order, fs, cutoff):
     b, a = signal.butter(order, normal_cutoff, btype='high', analog=False)
     return b, a
 
+
 def signal_filtration(data, order, fs, cutoff):
     b, a = filter(order, fs, cutoff)
-    y = signal.filtfilt(b, a, data)
-    return y
+    filtered_signal = pd.DataFrame(columns=["x", "y", "z", "time"])
+    filtered_signal["x"] = signal.filtfilt(b, a, data['x'])
+    filtered_signal["y"] = signal.filtfilt(b, a, data['y'])
+    filtered_signal["z"] = signal.filtfilt(b, a, data['z'])
+    return filtered_signal
 
 
-def signal_spectometer(data, timeStep ):
-
-
+def signal_spectometer(data, timeStep):
     yf = fft(data)
     xf = fftfreq(len(data), d=timeStep)
 
@@ -58,109 +113,96 @@ def signal_spectometer(data, timeStep ):
     plt.show()
 
 
-
-
 # reads multiple csv files to one dataFrame
-def readMultipleCSVdata(path, global_features_x, global_features_y, global_features_z):
-    data_files = glob.glob(f'{path}/*.csv')
-    # data = []
-    for filename in data_files:
-        df = pd.read_csv(filename, names=['x', 'y', 'z', 'time'])
+def preprocessing(data_files, global_features_x, global_features_y, global_features_z, labels):
+    # extract the filenames from path
+    data_names = []
+    for d in data_files:
+        headtail = os.path.split(d)
+        data_names.append(headtail[1])
 
+    # create dataframe for statistical parameters of the signal
+    signals = pd.DataFrame(data=[], index=[data_names],
+                           columns=["mean_x", "std_x", "mean_y", "std_y", "mean_z", "std_z", "label"])
+    # data = []
+    for filepath, filename in zip(data_files, data_names):
+        df = pd.read_csv(filepath, names=['x', 'y', 'z', 'time'])
+
+        # normalize the signal from certain file - NOT all at the same time
         normalized_df = normalize(df, global_features_x, global_features_y, global_features_z)
 
+        # count some time related parameters
+        duration = float((df['time'][[len(df['time']) - 1]] - df['time'][0]))
+        timeStep = float((duration / len(df['time'])))
+        fs = float(1 / timeStep)
+        sampleRate = len(df) / duration
 
-
-
-
-
-
-        duration = float((df['time'][[len(df['time'])-1]]-df['time'][0]))
-        timeStep = float(((df['time'][[len(df['time'])-1]]-df['time'][0])/len(df['time'])))
-        fs = float(1/timeStep)
-        sampleRate = len(df)/duration
+        # filter parameters
         order = 5
         cutoff = 2
-        x_filtered = signal_filtration(df['x'], order, fs, cutoff)
-        y_filtered = signal_filtration(df['y'], order, fs, cutoff)
-        z_filtered = signal_filtration(df['z'], order, fs, cutoff)
-        plt.plot(df['time'], df['x'], label='x')
-        plt.plot(df['time'], df['y'], label='y')
-        plt.plot(df['time'], df['z'], label='z')
-        plt.title(filename)
-        plt.xlabel('time')
-        plt.legend()
-        plt.show()
 
-        plt.plot(df['time'], normalized_df['x'], label='x-filtered')
-        plt.plot(df['time'], normalized_df['y'], label='y-filtered')
-        plt.plot(df['time'], normalized_df['z'], label='z-filtered')
-        plt.title(filename)
-        plt.xlabel('time')
-        plt.legend()
-        plt.show()
+        # signal filtration
+        filtered_df = signal_filtration(normalized_df, order, fs, cutoff)
 
-        # plt.plot(df['time'], x_filtered, label='x-filtered')
-        # plt.plot(df['time'], y_filtered, label='y-filtered')
-        # plt.plot(df['time'], z_filtered, label='z-filtered')
+        # count parameters of each signals and save to dataframe
+        signals = find_local_features(filtered_df, filename, signals)
+        signals['label'][filename] = labels['class'][filename]
+
+        # # plot raw signal
+        # plt.plot(df['time'], df['x'], label='x')
+        # plt.plot(df['time'], df['y'], label='y')
+        # plt.plot(df['time'], df['z'], label='z')
         # plt.title(filename)
         # plt.xlabel('time')
         # plt.legend()
         # plt.show()
-        signal_spectometer(x_filtered, timeStep)
-        print('ok')
+        #
+        # # plot normalized signal
+        # plt.plot(df['time'], normalized_df['x'], label='x-filtered')
+        # plt.plot(df['time'], normalized_df['y'], label='y-filtered')
+        # plt.plot(df['time'], normalized_df['z'], label='z-filtered')
+        # plt.title(filename)
+        # plt.xlabel('time')
+        # plt.legend()
+        # plt.show()
+        #
+        # # plot filtered signal
+        # plt.plot(df['time'], filtered_df['x'], label='x-filtered')
+        # plt.plot(df['time'], filtered_df['y'], label='y-filtered')
+        # plt.plot(df['time'], filtered_df['z'], label='z-filtered')
+        # plt.title(filename)
+        # plt.xlabel('time')
+        # plt.legend()
+        # plt.show()
+        #
+        # # plot a power spectrum of the signal
+        # signal_spectometer(filtered_df['x'], timeStep)
+
+        print(f'{filename} done')
+    return signals
 
 
-# find feature for all signal together
-def find_features(path):
-    data_files = glob.glob(f'{path}/*.csv')
-    global_description_x = pd.DataFrame(data=[], index=[
-                               "count", "mean", "std", "min", "25%", "50%", "75%", "max"], columns=[])
-    global_description_y = pd.DataFrame(data=[], index=[
-                               "count", "mean", "std", "min", "25%", "50%", "75%", "max"], columns=[])
-    global_description_z = pd.DataFrame(data=[], index=[
-                               "count", "mean", "std", "min", "25%", "50%", "75%", "max"], columns=[])
-    for filename in data_files:
-        df = pd.read_csv(filename, names=['x', 'y', 'z', 'time'])
-        description_i = df.describe()
-        global_description_x[filename] = description_i["x"]
-        global_features_x = global_description_x.mean(axis=1)
-        
-        global_description_y[filename] = description_i["y"]
-        global_features_y = global_description_y.mean(axis=1)
+# NOT WORKING YET
+def training(df):
+        X = df.loc[:, df.columns != 'label']
+        Y = df['label']
+        X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=25)
+        svc = SVC(kernel='linear')
+        svc.fit(X_train, y_train)
+        y_predict = svc.predict(X_test)
+        print('svm')
 
-        global_description_z[filename] = description_i["z"]
-        global_features_z = global_description_z.mean(axis=1)
-
-    return global_features_x, global_features_y, global_features_z
-
-def normalize(data, global_features_x, global_features_y, global_features_z):
-   
-    mean_x = float(global_features_x['mean'])
-    std_x = float(global_features_x['std'])
-    mean_y = float(global_features_y['mean'])
-    std_y = float(global_features_y['std'])
-    mean_z = float(global_features_z['mean'])
-    std_z = float(global_features_z['std'])
-    
-    normalised_signal= pd.DataFrame(columns = ["x","y","z"])
-    normalised_signal["x"]  = (data["x"] - mean_x)/std_x
-     
-    normalised_signal["y"]  = (data["y"] - mean_y)/std_y
-  
-    normalised_signal["z"]  = (data["z"] - mean_z)/std_z
-    normalised_signal['time'] = data['time']
-    return normalised_signal
 
 
 
 if __name__ == '__main__':
-    class_data = readCSVdata('./IFMBE Scientific Challenge/file_3classes.csv', id_col=0)
+    class_data = readCSVdata('./IFMBE Scientific Challenge/file_3classes.csv', id_col=1)
 
-    #changed_class_data = classChange(class_data)
+    # changed_class_data = classChange(class_data)
+
     path = './IFMBE Scientific Challenge/Train2'
-    global_features_x, global_features_y, global_features_z = find_features(path)
-    readMultipleCSVdata(path, global_features_x, global_features_y, global_features_z)
-
-
+    data_files = glob.glob(f'{path}/*.csv')
+    global_features_x, global_features_y, global_features_z = find_global_features(data_files)
+    signals_parameters_df = preprocessing(data_files, global_features_x, global_features_y, global_features_z, class_data)
+    training(signals_parameters_df)
     print('done')
