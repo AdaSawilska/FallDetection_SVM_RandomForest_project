fffc926678c5e6fbd1521678c6ffc0382e1a1faa
AdaSawilska <01143808@pw.edu.pl>
2023-05-22 21:33:37 +0200

    implementation SVM with linear kernel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import scipy
+from pandas.plotting import scatter_matrix
 from scipy import signal
 from scipy.fft import fft, fftfreq
 from scipy.signal import spectrogram
@@ -10,6 +11,7 @@ import matplotlib.pyplot as plt
 from sklearn import svm
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix, accuracy_score
 
 
 # reads the csv files to dataFrame
@@ -63,10 +65,17 @@ def find_local_features(df, filename, signals_dataframe):
     description_i = df.describe()
     signals_dataframe['mean_x'][filename] = description_i['x']['mean']
     signals_dataframe['std_x'][filename] = description_i['x']['std']
+    signals_dataframe['min_x'][filename] = description_i['x']['min']
+    signals_dataframe['max_x'][filename] = description_i['x']['max']
     signals_dataframe['mean_y'][filename] = description_i['y']['mean']
     signals_dataframe['std_y'][filename] = description_i['y']['std']
+    signals_dataframe['min_y'][filename] = description_i['y']['min']
+    signals_dataframe['max_y'][filename] = description_i['y']['max']
     signals_dataframe['mean_z'][filename] = description_i['z']['mean']
     signals_dataframe['std_z'][filename] = description_i['z']['std']
+    signals_dataframe['min_z'][filename] = description_i['z']['min']
+    signals_dataframe['max_z'][filename] = description_i['z']['max']
+
     return signals_dataframe
 
 
@@ -123,7 +132,8 @@ def preprocessing(data_files, global_features_x, global_features_y, global_featu
 
     # create dataframe for statistical parameters of the signal
     signals = pd.DataFrame(data=[], index=[data_names],
-                           columns=["mean_x", "std_x", "mean_y", "std_y", "mean_z", "std_z", "label"])
+                           columns=["mean_x", "std_x", "min_x", "max_x", "mean_y", "std_y", "min_y", "max_y", "mean_z",
+                                    "std_z", "min_z", "max_z", "class"])
     # data = []
     for filepath, filename in zip(data_files, data_names):
         df = pd.read_csv(filepath, names=['x', 'y', 'z', 'time'])
@@ -146,7 +156,8 @@ def preprocessing(data_files, global_features_x, global_features_y, global_featu
 
         # count parameters of each signals and save to dataframe
         signals = find_local_features(filtered_df, filename, signals)
-        signals['label'][filename] = labels['class'][filename]
+        signals['class'][filename] = labels['class'][filename]
+
 
         # # plot raw signal
         # plt.plot(df['time'], df['x'], label='x')
@@ -179,17 +190,30 @@ def preprocessing(data_files, global_features_x, global_features_y, global_featu
         # signal_spectometer(filtered_df['x'], timeStep)
 
         print(f'{filename} done')
+    signals = signals.astype(float)
+    dataset = signals.drop('class', axis=1)
+    scatter_matrix(dataset, figsize=(16, 9))
+    plt.show()
     return signals
 
 
 # NOT WORKING YET
 def training(df):
-        X = df.loc[:, df.columns != 'label']
-        Y = df['label']
+        Y = df['class']
+        X = df.drop('class', axis=1)
         X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=25)
         svc = SVC(kernel='linear')
         svc.fit(X_train, y_train)
-        y_predict = svc.predict(X_test)
+        y_pred = svc.predict(X_test)
+
+        # Evaluating the accuracy of the model using the sklearn functions
+        accuracy = accuracy_score(y_test.values, y_pred) * 100
+        confusion_mat = confusion_matrix(y_test.values, y_pred)
+
+        # Printing the results
+        print("Accuracy for SVM is:", accuracy)
+        print("Confusion Matrix")
+        print(confusion_mat)
         print('svm')
 
 
@@ -197,6 +221,14 @@ def training(df):
 
 if __name__ == '__main__':
     class_data = readCSVdata('./IFMBE Scientific Challenge/file_3classes.csv', id_col=1)
+
+    # url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+    # names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+    # dataset = pd.read_csv(url, names=names)
+    # dataset = dataset.drop('class', axis=1)
+    # scatter_matrix(dataset, figsize=(16, 9))
+    # plt.show()
+
 
     # changed_class_data = classChange(class_data)
 
